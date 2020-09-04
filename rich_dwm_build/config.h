3a086f4c9a9e027092ffd3de364b858a07530c7f
Rich Colquhoun <rich@trumpetpla.net>
2020-09-04 15:38:57 +0100

    Update README and dmenu colours

--- rich_dwm_build/config.h
+++ rich_dwm_build/config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const int gappx		    = 16; 	/* set default gaps */
 static const int swallowfloating    = 0;	/* 1 means swallow floating by default */
 static const char *fonts[]	    = { "Inconsolata:size=11:antialias=true:autohint=true", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
