67f95546ee9314ccd08f1c41dd790ef08b2af777
Rich Colquhoun <rich@trumpetpla.net>
2020-12-19 14:34:59 +0000

    current settings, no build change

--- rich_dwm_build/config.h
+++ rich_dwm_build/config.h
@@ -1,26 +1,27 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 16;       /* snap pixel */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int snap      = 4;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const int gappx		    = 16; 	/* set default gaps */
-static const int swallowfloating    = 0;	/* 1 means swallow floating by default */
-static const char *fonts[]	    = { "Inconsolata:size=11:antialias=true:autohint=true", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
-static const char dmenufont[]	    = "Inconsolata:size=11:antialias=true:autohint=true";
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const int gappx		    = 24; 	/* set default gaps */
+static const int swallowfloating    = 1;	/* 1 means swallow floating by default */
+static const char *fonts[]	    = { "Lato Heavy:size=10:antialias=true:autohint=true",
+					"JoyPixels:pixelsize=11:antialias=true:autohint=true" };
+static const char dmenufont[]	    = "Lato Heavy:size=10:antialias=true:autohint=true";
 static const char col_white[]	    = "#ffffff";
 static const char col_ltgrey[]	    = "#eeeeee";
 static const char col_grey0[]	    = "#303030";
 static const char col_grey1[]       = "#161616";
 static const char col_black[]	    = "#000000";
 static const char col_termblu[]	    = "#282A36";
-/* static const char col_yelloo[]   = "#ffcc00";
-   static const char col_haiku[]    = "#315683"; */
+static const char col_yelloo[]      = "#ffcc00";
+static const char col_haiku[]       = "#315683";
 static const char *colors[][3]      = {
-/*                       bar text,  bar color,   win border */
-	[SchemeNorm] = { col_ltgrey, col_termblu, col_termblu },
-	[SchemeSel]  = { col_white, col_termblu,  col_termblu }, };
+/*                       bar text,   bar color,   win border */
+	[SchemeNorm] = { col_yelloo, col_termblu, col_termblu },
+	[SchemeSel]  = { col_yelloo, col_termblu, col_termblu }, };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "🗑" };
@@ -51,7 +52,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_termblu, "-nf", col_white, "-sb", col_ltgrey, "-sf", col_black, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_termblu, "-nf", col_yelloo, "-sb", col_yelloo, "-sf", col_black, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "shiftview.c"
