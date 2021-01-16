622e7ee7312bddd23fec6c6c8500d80f7269c253
Rich Colquhoun <rich@trumpetpla.net>
2021-01-16 14:57:07 +0000

    Big change to "version 0.3"

--- rich_dwm_build/config.h
+++ rich_dwm_build/config.h
@@ -2,45 +2,49 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 4;       /* snap pixel */
+static const int swallowfloating    = 1;	/* swallowing on */
+static const int gappx		    = 0;	/* initial window gaps */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int gappx		    = 24; 	/* set default gaps */
-static const int swallowfloating    = 1;	/* 1 means swallow floating by default */
-static const char *fonts[]	    = { "Lato Heavy:size=10:antialias=true:autohint=true",
-					"JoyPixels:pixelsize=11:antialias=true:autohint=true" };
-static const char dmenufont[]	    = "Lato Heavy:size=10:antialias=true:autohint=true";
+static const char *fonts[]	    = { "JetBrainsMono-Regular:size=9:antialias=true:autohint=true",
+					"JoyPixels:pixelsize=9:antialias=true:autohint=true" };
+static const char dmenufont[]	    = "JetBrainsMono-Regular:size=9:antialias=true:autohint=true";
 static const char col_white[]	    = "#ffffff";
 static const char col_ltgrey[]	    = "#eeeeee";
-static const char col_grey0[]	    = "#303030";
-static const char col_grey1[]       = "#161616";
+static const char col_dkgrey[]	    = "#111111";
 static const char col_black[]	    = "#000000";
 static const char col_termblu[]	    = "#282A36";
-static const char col_yelloo[]      = "#ffcc00";
-static const char col_haiku[]       = "#315683";
+static const char col_newblu[]      = "#6666ee";
+static const char col_dkgreen[]	    = "#006600";
+static const char col_yelloo[]	    = "#ffcc00";
 static const char *colors[][3]      = {
-/*                       bar text,   bar color,   win border */
-	[SchemeNorm] = { col_yelloo, col_termblu, col_termblu },
-	[SchemeSel]  = { col_yelloo, col_termblu, col_termblu }, };
+/*                       bar text,  bar color,   win border */
+	[SchemeNorm] = { col_ltgrey, col_dkgrey, col_termblu },
+	[SchemeSel]  = { col_white, col_termblu,  col_termblu }, };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "🗑" };
+static const char *tags[] = { "1", "2", "3", "4", "[ 🗑 ]" };
 
 static const Rule rules[] = {
-	/* class    instance  title  tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",   NULL,     NULL,  1 << 4,    1,          0,          0,         -1 },
-	{ "st",	    NULL,     NULL,  0,		0,	    1, 		0,	   -1 }, };
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance  title  tags mask  isfloating  isterminal  noswallow  monitor */
+/*	{ "Gimp",     NULL,     NULL,  0,         1,          0,          0,         -1 }, */
+	{ "st",	      NULL,     NULL,  0,         0,          1,          0,         -1 }, };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.65; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "🐧",      tile },    /* first layout is default */
-	{ "👽",      NULL },    /* no layout function means floating behavior */
-	{ "🔍",      monocle }, };
+	{ "[ 👽 ]",      tile },    /* first layout is default */
+	{ "[ 👻 ]",      NULL },    /* no layout function means floating behavior */
+	{ "[ 🔍 ]",      monocle }, };
 
 /* key definitions */
 #define MODKEY Mod4Mask
@@ -50,14 +54,19 @@ static const Layout layouts[] = {
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_termblu, "-nf", col_yelloo, "-sb", col_yelloo, "-sf", col_black, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
+				"-nb", col_dkgrey, "-nf", col_ltgrey,
+				"-sb", col_newblu, "-sf", col_black, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "shiftview.c"
 static Key keys[] = {
-	/* modifier                 key        function        argument */
+	/* modifier                     key        function        argument */
 	{ MODKEY,                   XK_space,  	spawn,          {.v = dmenucmd } },
 	{ MODKEY,	            XK_Return, 	spawn,          {.v = termcmd } },
 
@@ -71,12 +80,13 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,         XK_Return, 	zoom,           {0} },
 	{ MODKEY,                   XK_Tab,    	view,           {0} },
 
-	{ MODKEY,                   XK_minus,  	setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,         XK_minus,  	setlayout,      {.v = &layouts[1]} },
-/*	{ MODKEY,                   XK_equal,  	setlayout,      {.v = &layouts[2]} }, */
-	{ MODKEY,		    XK_equal, 	fullscreen,	{0} },
+	{ MODKEY,		    XK_z,       setgaps,       {.i = -2 } },
+        { MODKEY,		    XK_x,       setgaps,       {.i = +2 } },
 
-/*	{ MODKEY,                   XK_space,  	setlayout,      {0} }, */
+	{ MODKEY,                   XK_equal,  	setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,	            XK_minus,  	setlayout,      {.v = &layouts[1]} },
+/*	{ MODKEY,                   XK_equal,  	setlayout,      {.v = &layouts[2]} },*/
+
 	{ MODKEY|ShiftMask,         XK_space,  	togglefloating, {0} },
 	{ MODKEY,                   XK_0,      	view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,         XK_0,      	tag,            {.ui = ~0 } },
@@ -84,12 +94,7 @@ static Key keys[] = {
 	{ MODKEY,                   XK_period, 	focusmon,       {.i = +1 } },
  	{ MODKEY|ShiftMask,         XK_comma,  	tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,         XK_period, 	tagmon,         {.i = +1 } },
-	{ MODKEY,		    XK_x,  	setgaps,        {.i = +2 } },
-	{ MODKEY,		    XK_z,  	setgaps,	{.i = -2 } },
-/*	{ MODKEY|ShiftMask,	    XK_equal,  	setgaps,        {.i = 0  } },
-	{ MODKEY|ShiftMask,	    XK_minus,  	setgaps,	{.i = 18 } }, */
 
-	{ MODKEY|ShiftMask,         XK_c,     	killclient,     {0} },
 	{ MODKEY,		    XK_q,	killclient,	{0} },
 	{ MODKEY|ShiftMask,         XK_q,       quit,           {0} },
 
@@ -97,11 +102,11 @@ static Key keys[] = {
 	TAGKEYS( XK_2, 1)
 	TAGKEYS( XK_3, 2)
 	TAGKEYS( XK_4, 3)
-	TAGKEYS( XK_5, 4)
+/*	TAGKEYS( XK_5, 4)
 	TAGKEYS( XK_6, 5)
 	TAGKEYS( XK_7, 6)
-	TAGKEYS( XK_8, 7)
-	TAGKEYS( XK_9, 8) };
+	TAGKEYS( XK_8, 7)*/
+	TAGKEYS( XK_9, 4) };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
@@ -118,5 +123,6 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-	{ ClkTagBar,		0,		Button4,	shiftview,      {.i = +1} },
-	{ ClkTagBar,		0,		Button5,	shiftview,	{.i = -1} }, };
+	{ ClkTagBar,		0,		Button4,	shiftview,	{.i = +1 } },
+	{ ClkTagBar,		0,		Button5,	shiftview,	{.i = -1 } },
+};
--- rich_dwm_build/dwm.c
+++ rich_dwm_build/dwm.c
@@ -206,7 +206,6 @@ static void sendmon(Client *c, Monitor *m);
 static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
-static void fullscreen(const Arg *arg);
 static void setgaps(const Arg *arg);
 static void setlayout(const Arg *arg);
 static void setmfact(const Arg *arg);
@@ -492,7 +491,7 @@ unswallow(Client *c)
 void
 buttonpress(XEvent *e)
 {
-	unsigned int i, x, click, occ = 0;
+	unsigned int i, x, click;
 	Arg arg = {0};
 	Client *c;
 	Monitor *m;
@@ -507,14 +506,9 @@ buttonpress(XEvent *e)
 	}
 	if (ev->window == selmon->barwin) {
 		i = x = 0;
-		for (c = m->clients; c; c = c->next)
-			occ |= c->tags == 255 ? 0 : c->tags;
-		do {
-			/* do not reserve space for vacant tags */
-			if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
-				continue;
+		do
 			x += TEXTW(tags[i]);
-		} while (ev->x >= x && ++i < LENGTH(tags));
+		while (ev->x >= x && ++i < LENGTH(tags));
 		if (i < LENGTH(tags)) {
 			click = ClkTagBar;
 			arg.ui = 1 << i;
@@ -602,7 +596,7 @@ clientmessage(XEvent *e)
 		if (cme->data.l[1] == netatom[NetWMFullscreen]
 		|| cme->data.l[2] == netatom[NetWMFullscreen])
 			setfullscreen(c, (cme->data.l[0] == 1 /* _NET_WM_STATE_ADD    */
-				|| cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */));
+				|| (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */ && !c->isfullscreen)));
 	} else if (cme->message_type == netatom[NetActiveWindow]) {
 		if (c != selmon->sel && !c->isurgent)
 			seturgent(c, 1);
@@ -632,6 +626,7 @@ void
 configurenotify(XEvent *e)
 {
 	Monitor *m;
+	Client *c;
 	XConfigureEvent *ev = &e->xconfigure;
 	int dirty;
 
@@ -644,6 +639,9 @@ configurenotify(XEvent *e)
 			drw_resize(drw, sw, bh);
 			updatebars();
 			for (m = mons; m; m = m->next) {
+				for (c = m->clients; c; c = c->next)
+					if (c->isfullscreen)
+						resizeclient(c, m->mx, m->my, m->mw, m->mh);
 				XMoveResizeWindow(dpy, m->barwin, m->wx, m->by, m->ww, bh);
 			}
 			focus(NULL);
@@ -790,19 +788,19 @@ drawbar(Monitor *m)
 	}
 
 	for (c = m->clients; c; c = c->next) {
-		occ |= c->tags == 255 ? 0 : c->tags;
+		occ |= c->tags;
 		if (c->isurgent)
 			urg |= c->tags;
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
-		/* do not draw vacant tags */
-		if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
-		continue;
-
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
+		if (occ & 1 << i)
+			drw_rect(drw, x + boxs, boxs, boxw, boxw,
+				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
+				urg & 1 << i);
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
@@ -1228,6 +1226,8 @@ movemouse(const Arg *arg)
 
 	if (!(c = selmon->sel))
 		return;
+	if (c->isfullscreen) /* no support moving fullscreen windows by mouse */
+		return;
 	restack(selmon);
 	ocx = c->x;
 	ocy = c->y;
@@ -1380,6 +1380,8 @@ resizemouse(const Arg *arg)
 	Time lasttime = 0;
 
 	if (!(c = selmon->sel))
+		return;
+	if (c->isfullscreen) /* no support resizing fullscreen windows by mouse */
 		return;
 	restack(selmon);
 	ocx = c->x;
@@ -1562,10 +1564,24 @@ setfullscreen(Client *c, int fullscreen)
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)&netatom[NetWMFullscreen], 1);
 		c->isfullscreen = 1;
+		c->oldstate = c->isfloating;
+		c->oldbw = c->bw;
+		c->bw = 0;
+		c->isfloating = 1;
+		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
+		XRaiseWindow(dpy, c->win);
 	} else if (!fullscreen && c->isfullscreen){
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)0, 0);
 		c->isfullscreen = 0;
+		c->isfloating = c->oldstate;
+		c->bw = c->oldbw;
+		c->x = c->oldx;
+		c->y = c->oldy;
+		c->w = c->oldw;
+		c->h = c->oldh;
+		resizeclient(c, c->x, c->y, c->w, c->h);
+		arrange(c->mon);
 	}
 }
 
@@ -1577,19 +1593,6 @@ setgaps(const Arg *arg)
 	else
 		selmon->gappx += arg->i;
 	arrange(selmon);
-}
-
-Layout *last_layout;
-void
-fullscreen(const Arg *arg)
-{
-	if (selmon->showbar) {
-		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
-		setlayout(&((Arg) { .v = &layouts[2] }));
-	} else {
-		setlayout(&((Arg) { .v = last_layout }));
-	}
-	togglebar(arg);
 }
 
 void
@@ -1713,7 +1716,7 @@ showhide(Client *c)
 	if (ISVISIBLE(c)) {
 		/* show clients top down */
 		XMoveWindow(dpy, c->win, c->x, c->y);
-		if (!c->mon->lt[c->mon->sellt]->arrange || c->isfloating)
+		if ((!c->mon->lt[c->mon->sellt]->arrange || c->isfloating) && !c->isfullscreen)
 			resize(c, c->x, c->y, c->w, c->h, 0);
 		showhide(c->snext);
 	} else {
@@ -1804,6 +1807,8 @@ void
 togglefloating(const Arg *arg)
 {
 	if (!selmon->sel)
+		return;
+	if (selmon->sel->isfullscreen) /* no support for fullscreen windows */
 		return;
 	selmon->sel->isfloating = !selmon->sel->isfloating || selmon->sel->isfixed;
 	if (selmon->sel->isfloating)
